dfb2ac39c664a5390356877902e62521f2c91b41
Pierre M <devleaks.be@gmail.com>
2022-04-19 13:48:04 +0200

    first attempt at dockerizing. looks more like art than docker

---
+++ src/emitpy/parameters-docker.py
@@ -0,0 +1,35 @@
+"""
+Application parameters not related to the simulation.
+Used for file location, connection details, etc.
+"""
+import os
+
+DEVELOPMENT = False  # produces additional debug
+PRODUCTION = True  # removed caches and short circuits
+
+HOME_DIR = "/app"
+
+# DATA is a database of static data, definitions, etc.
+DATA_DIR = os.path.join(HOME_DIR, "data")
+
+# AODB is a database of working data
+AODB_DIR = os.path.join(HOME_DIR, "db")
+
+
+METAR_URL = "http://tgftp.nws.noaa.gov/data/observations/metar/stations"
+
+MANAGED_AIRPORT = {
+    "ICAO": "OTHH",
+    "IATA": "DOH",
+    "name": "Hamad International Airport",
+    "city": "Doha",
+    "country": "Qatar",
+    "regionName": "Qatar",
+    "elevation": 13.000000019760002,
+    "lat": 25.2745,
+    "lon": 51.6077,
+    "tzoffset": 3,
+    "tzname": "Doha"
+}
+
+LOAD_AIRWAYS=True  # to speedup developments
