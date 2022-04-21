47b5c58f4fe094bd70a31a2b8ffe41fe5f68bdfb
Pierre Mareschal <pierre@devleaks.be>
2022-04-21 16:43:58 +0200

    create special docker version

--- src/emitpy/__init__.py
+++ src/emitpy/__init__.py
@@ -1,4 +1,5 @@
-__version__ = "0.3.2"
+__version__ = "0.4.0"
 __version_info__ = tuple(map(int, __version__.split(".")))
+__version_name__ = "Moskva"
 __NAME__ = "emitpy"
 __DESCRIPTION__ = "Flight and turnaround ground support equipment track generator."
