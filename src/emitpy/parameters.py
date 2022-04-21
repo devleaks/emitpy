e136cc002f2632147c01f7242e20b28132c763a0
Pierre M <devleaks.be@gmail.com>
2022-04-21 16:23:26 +0200

    saved docker version of params

--- src/emitpy/parameters.py
+++ src/emitpy/parameters.py
@@ -7,7 +7,7 @@ import os
 DEVELOPMENT = False  # produces additional debug
 PRODUCTION = True  # removed caches and short circuits
 
-HOME_DIR = os.path.join(".", "..")
+HOME_DIR = os.path.join("/", "app")  # should work even on windows... python guys are genius.
 
 # DATA is a database of static data, definitions, etc.
 DATA_DIR = os.path.join(HOME_DIR, "data")
@@ -16,7 +16,7 @@ DATA_DIR = os.path.join(HOME_DIR, "data")
 AODB_DIR = os.path.join(HOME_DIR, "db")
 
 
-METAR_URL = "http://tgftp.nws.noaa.gov/data/observations/metar/stations"
+METAR_URL = "http://tgftp.nws.noaa.gov/data/observations/metar/stations"  # on window, don't you have to change / to \?
 
 MANAGED_AIRPORT = {
     "ICAO": "OTHH",
@@ -25,7 +25,7 @@ MANAGED_AIRPORT = {
     "city": "Doha",
     "country": "Qatar",
     "regionName": "Qatar",
-    "elevation": 13.000000019760002,
+    "elevation": 13.0,
     "lat": 25.2745,
     "lon": 51.6077,
     "tzoffset": 3,
@@ -35,15 +35,15 @@ MANAGED_AIRPORT = {
 
 # Default queues are created in emitpy if they do not exists.
 DEFAULT_QUEUES = {
-    "lt": "lt",
     "raw": "raw"
 }
 
 # REDIS stuff if not on same host
 REDIS_CONNECT = {
-    "host": "localhost", # "redis",
+    "host": "redis",
     "port": 6379,
     "db": 0
 }
 
-LOAD_AIRWAYS=False  # to speedup developments
+LOAD_AIRWAYS=True  # to speedup developments
+
