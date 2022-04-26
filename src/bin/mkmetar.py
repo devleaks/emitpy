a31d32d95376780cee6c35873b4466d35abc31e4
Pierre Mareschal <pierre@devleaks.be>
2022-04-26 14:50:05 +0200

    refactored metar for several input channels

--- src/bin/mkmetar.py
+++ src/bin/mkmetar.py
@@ -2,9 +2,17 @@
 Test API for flightplandatabase with request caching.
 
 """
+import sys
+sys.path.append('/Users/pierre/Developer/oscars/emitpy/src')
+import logging
+
+
 from emitpy.airspace.metar import Metar
 from emitpy.utils import Timezone
 from datetime import datetime
+
+logging.basicConfig(level=logging.DEBUG)
+
 
 dohatime = Timezone(offset=3, name="Doha")
 
@@ -12,8 +20,9 @@ dohatime = Timezone(offset=3, name="Doha")
 def main():
 
     dt = datetime.strptime("2019-04-01 02:25:00", "%Y-%m-%d %H:%M:%S").replace(tzinfo=dohatime)
-    m = Metar(icao="OTHH", use_redis=True)
-    print(m.metar)
+    m = Metar.new(icao="OTHH", method="MetarHistorical")
+    m.setDatetime(moment=dt)
+    print(m.metar, m.getInfo())
 
 
 if __name__ == "__main__":
