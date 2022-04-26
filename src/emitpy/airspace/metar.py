7117af57d1d84f940c80320751349461e2f5b6b1
Pierre Mareschal <pierre@devleaks.be>
2022-04-26 14:50:48 +0200

    refactored metar for several input channels

--- src/emitpy/airspace/metar.py
+++ src/emitpy/airspace/metar.py
@@ -8,6 +8,7 @@ import logging
 import requests_cache
 import redis
 import urllib.request
+import importlib
 
 from datetime import datetime, timedelta, timezone
 from metar import Metar as MetarLib
@@ -17,7 +18,7 @@ import flightplandb as fpdb
 # from metar import Metar as MetarLib
 
 from ..constants import METAR_DATABASE
-from ..parameters import AODB_DIR, REDIS_CONNECT
+from ..parameters import AODB_DIR, REDIS_CONNECT, USE_REDIS
 
 from ..private import FLIGHT_PLAN_DATABASE_APIKEY
 
@@ -25,6 +26,8 @@ METAR_DIR = os.path.join(AODB_DIR, METAR_DATABASE)
 
 logger = logging.getLogger("Metar")
 
+
+METAR_DIR = os.path.join(AODB_DIR, METAR_DATABASE)
 
 def round_dt(dt, delta):  # rounds date to delta after date.
     return dt + (datetime.min - dt.replace(tzinfo=None)) % delta
@@ -36,53 +39,155 @@ def normalize_dt(dt):
     return dtret
 
 
-class MetarHistorical(object):
-    """
-    Wrapper to maintain symmetry with current metar
-    """
-    def __init__(self, metar: str):
-        self.METAR = metar
-        self.TAF = None
-    def _to_api_dict(self):
-        return {"METAR": self.METAR}
-
-
 class Metar:
     """
     Loads cached METAR for ICAO or fetch **current** from flightplandatabase.
     """
-
-    USE_REDIS = False
-
-    def __init__(self, icao: str, moment: datetime = None, use_redis: bool = False):
+    def __init__(self, icao: str):
         self.icao = icao
+        self.moment = datetime.now()
+        self.moment_norm = normalize_dt(self.moment)
+        self.metar = None   # parsed metar
+        self.raw = None     # metar string
+
+    @staticmethod
+    def new(icao: str, method: str = "MetarFPDB"):
+        metarclasses = importlib.import_module(name=".airspace.metar", package="emitpy")
+        if hasattr(metarclasses, method):
+            doit = getattr(metarclasses, method)
+            return doit(icao)
+        else:
+            logger.warning(f":__init__: could not get Metar implementation {method}")
+        return None
+
+    def init(self):
+        self.load()
+        if self.raw is None:
+            self.fetch()
+            self.save()
+
+    def setDatetime(self, moment: datetime = datetime.now()):
         self.moment = moment
-        self.raw = None
+        self.moment_norm = normalize_dt(self.moment)
         self.metar = None
-        self.api = fpdb.FlightPlanDB(FLIGHT_PLAN_DATABASE_APIKEY)
-
-        # For development
-        requests_cache.install_cache()
-
-        if use_redis:  # sets it once and for all
-            Metar.USE_REDIS = use_redis
-
-        self.moment_norm = normalize_dt(self.moment if self.moment is not None else datetime.now())
-
+        self.raw = None
         self.init()
 
 
-    def init(self):
-        if Metar.USE_REDIS:
-            self.loadDB()
-            if self.raw is None:
-                self.fetch()
-                self.saveDB()
+    def get(self):
+        return self.raw
+
+
+    def getInfo(self):
+        return {
+            "icao": self.icao,
+            "date": self.moment_norm.isoformat(),
+            "metar": self.raw
+        }
+
+
+    def fetch(self):
+        """
+        Fetches the metar from a source.
+        """
+        return (False, "Metar::fetch: abstract class")
+
+
+    def save(self):
+        if USE_REDIS:
+            return self.saveDB()
+        metid = "*ERROR*"
+        fn = "*ERROR*"
+        if self.raw is not None:
+            metid = self.raw.METAR[0:4] + '-' + self.raw.METAR[5:12]
+            fn = os.path.join(METAR_DIR, metid + ".json")
+            if not os.path.exists(fn):
+                with open(fn, "w") as outfile:
+                    print(self.raw, outfile)
+            else:
+                logger.warning(f":save: already exist {fn}")
+            return (True, "Metar::save: saved")
+        return (False, "Metar::save: not saved")
+
+
+    def load(self):
+        if USE_REDIS:
+            return self.loadDB()
+        nowstr = self.moment_norm.strftime('%d%H%MZ')
+        fn = os.path.join(METAR_DIR, self.icao + "-" + nowstr + ".json")
+        logger.debug(f":load: trying {fn}")
+        if os.path.exists(fn):
+            with open(fn, "r") as fp:
+                self.raw = read(fp)
+            if self.raw is not None:
+                logger.debug(f":load: found {fn}")
+                return self.parse(self.raw["METAR"])
+            return (False, "Metar::load: not loaded")
         else:
-            self.load()
-            if self.raw is None:
-                self.fetch()
-                self.save()
+            logger.debug(f":load: not found {fn}")
+
+
+    def saveDB(self):
+        if self.raw is not None:
+            nowstr = self.getFullDT()
+            metid = "METAR:" + self.raw[0:4] + ':' + nowstr
+            r = redis.Redis(**REDIS_CONNECT)
+            if not r.exists(metid):
+                r.set(metid, self.raw)
+                return (True, "Metar::saveDB: saved")
+            else:
+                logger.warning(f":saveDB: alresaady exist {metid}")
+        return (False, "Metar::saveDB: not saved")
+
+
+    def loadDB(self):
+        nowstr = self.getFullDT()
+        metid = "METAR:" + self.icao + ":" + nowstr
+        logger.debug(f":loadDB: trying {metid}")
+        r = redis.Redis(**REDIS_CONNECT)
+        if r.exists(metid):
+            raw = r.get(metid)
+            self.raw = raw.decode("UTF-8")
+            if self.raw is not None:
+                return self.parse()
+            return (False, "Metar::loadDB: failed to get")
+        else:
+            logger.debug(f":loadDB: not found {metid}")
+        return (False, "Metar::loadDB: failed to load")
+
+
+    def getFullDT(self):
+        return self.moment_norm.strftime('%Y%m%d%H%MZ')
+
+
+    def parse(self):
+        """
+        Clear protected parsing of Metar.
+        If parsing succeeded, result is kept
+        """
+        try:
+            parsed = MetarLib.Metar(self.raw)
+            if parsed is not None:
+                self.metar = parsed
+            return (True, "Metar::parse: parsed")
+        except MetarLib.ParserError as e:
+            logger.debug(f":load: METAR failed to parse '{self.raw}': {e.message}")
+            parsed = None
+        return (False, "Metar::parse: failed to parse")
+
+
+
+class MetarFPDB(Metar):
+    """
+    Loads cached METAR for ICAO or fetch **current** from flightplandatabase.
+    """
+
+    def __init__(self, icao: str):
+        Metar.__init__(self, icao=icao)
+        self.api = fpdb.FlightPlanDB(FLIGHT_PLAN_DATABASE_APIKEY)
+        # For development
+        requests_cache.install_cache()
+        self.init()
 
 
     def fetch(self):
@@ -94,86 +199,23 @@ class Metar:
           "TAF": "TAF AMD KLAX 042058Z 0421/0524 26012G22KT P6SM SCT180 SCT250 FM050400 26007KT P6SM SCT200 FM050700 VRB05KT P6SM SCT007 SCT200 FM051800 23006KT P6SM SCT020 SCT180"
         }
         """
-        if self.moment is not None:
-            ret = self.fetchHistoricalMetar()
-            if ret[0]:
-                return ret
-            logger.warning(f":fetch: could not get historical metar, trying for now")
-        # if could not get historical metar, get latest one
         metar = self.api.weather.fetch(icao=self.icao)
         if metar is not None and metar.METAR is not None:
-            self.raw = metar
-            logger.debug(f":fetch: {self.raw.METAR},")
-            self.parse(self.raw.METAR)
-            return (True, "Metar::fetch: metar parsed")
-        return (False, "Metar::fetch: could not get metar")
+            self.raw = metar.METAR
+            logger.debug(f":fetch: {self.raw}")
+            return self.parse()
+        return (False, "MetarFPDB::fetch: could not get metar")
 
 
-    def save(self):
-        metid = "*ERROR*"
-        fn = "*ERROR*"
-        if self.raw is not None:
-            metid = self.raw.METAR[0:4] + '-' + self.raw.METAR[5:12]
-            fn = os.path.join(METAR_DIR, metid + ".json")
-            if not os.path.exists(fn):
-                with open(fn, "w") as outfile:
-                    json.dump(self.raw._to_api_dict(), outfile)
+class MetarHistorical(Metar):
+    """
+    Wrapper to maintain symmetry with current metar
+    """
+    def __init__(self, icao: str):
+        Metar.__init__(self, icao=icao)
 
 
-    def load(self):
-        nowstr = self.moment_norm.strftime('%d%H%MZ')
-        fn = os.path.join(METAR_DIR, self.icao + "-" + nowstr + ".json")
-        logger.debug(f":load: trying {fn}")
-        if os.path.exists(fn):
-            with open(fn, "r") as fp:
-                self.raw = json.load(fp)
-            if self.raw is not None:
-                self.parse(self.raw["METAR"])
-            logger.debug(f":load: found {fn}")
-        else:
-            logger.debug(f":load: not found {fn}")
-
-
-    def saveDB(self):
-        if self.raw is not None:
-            metid = "METAR:" + self.raw.METAR[0:4] + ':' + self.raw.METAR[5:12]
-            r = redis.Redis(**REDIS_CONNECT)
-            if not r.exists(metid):
-                r.set(metid, json.dumps(self.raw._to_api_dict()))
-
-
-    def loadDB(self):
-        nowstr = self.moment_norm.strftime('%d%H%MZ')
-        metid = "METAR:" + self.icao + ":" + nowstr
-        logger.debug(f":loadDB: trying {metid}")
-        r = redis.Redis(**REDIS_CONNECT)
-        if r.exists(metid):
-            raw = r.get(metid)
-            self.raw = json.loads(raw.decode("UTF-8"))
-            if self.raw is not None:
-                self.parse(self.raw["METAR"])
-            logger.debug(f":loadDB: loaded {metid}")
-        else:
-            logger.debug(f":loadDB: not found {metid}")
-
-
-    def get(self):
-        return None if self.raw is None else self.raw["METAR"]
-
-
-    def parse(self, metar: str):
-        parsed = None
-        try:
-            parsed = MetarLib.Metar(metar)
-        except MetarLib.ParserError as e:
-            logger.debug(f":load: METAR failed to parse '{metar}': {e.message}")
-            parsed = None
-
-        if parsed is not None:
-            self.metar = parsed
-
-
-    def fetchHistoricalMetar(self):
+    def fetch(self):
         """
         https://www.ogimet.com/display_metars2.php?lang=en&lugar=OTHH&tipo=SA&ord=REV&nil=SI&fmt=txt&ano=2019&mes=04&day=13&hora=07&anof=2019&mesf=04&dayf=13&horaf=07&minf=59&send=send      ==>
         201904130700 METAR OTHH 130700Z 29012KT 3500 TSRA FEW015 SCT030 FEW040CB OVC100 21/17 Q1011 NOSIG=
@@ -188,23 +230,21 @@ class Metar:
         url = f"https://www.ogimet.com/display_metars2.php?lang=en&lugar={self.icao}&tipo=SA&ord=REV&nil=SI&fmt=txt"
         url = url + f"&ano={yr}&mes={mo}&day={dy}&hora={hr}&anof={yr}&mesf={mo}&dayf={dy}&horaf={hr}&minf=59&send=send"
 
-        logger.debug(f":fetchHistoricalMetar: {url}")
+        #logger.debug(f":fetch: url={url}")
         #with open("/Users/pierre/Developer/oscars/emitpy/src/emitpy/airspace/result.txt", "r") as response:  # urllib.request.urlopen(url) as response:
         with urllib.request.urlopen(url) as response:
-            txt = response.read()  # .decode("UTF-8")
-            # logger.debug(f":fetchHistoricalMetar: {txt}")
+            txt = response.read().decode("UTF-8")
+            # logger.debug(f":fetch: {txt}")
             # 201903312300 METAR OTHH 312300Z
             start = f"{nowstr2} METAR {self.icao} {nowstr}"
+            logger.debug(f":fetch: start '{start}'")
             metar = None
             for line in re.findall(start+"(.*)", txt):
                  metar = start+line
             # logger.debug(f":fetchHistoricalMetar: search for '{start}(.*)': {metar}")
 
         if metar is None:
-            return (False, "failed to get historical metar")
+            return (False, "MetarHistorical::fetch: failed to get historical metar")
 
-        self.raw = MetarHistorical(metar=metar[len(nowstr2)+7:-1])
-        self.parse(self.raw.METAR)
-
-        return (True, "Metar::fetchHistoricalMetar: got historical metar")
-
+        self.raw = metar[len(nowstr2)+7:-1]
+        return self.parse()
