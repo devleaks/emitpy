024c28d8b554143b4aeefe65386fd6b106ee5d13
Pierre Mareschal <pierre@devleaks.be>
2022-05-20 10:46:26 +0200

    uses higher level requests package

--- src/emitpy/airspace/metar.py
+++ src/emitpy/airspace/metar.py
@@ -8,6 +8,7 @@ import logging
 import requests_cache
 import urllib.request
 import importlib
+import requests
 
 from datetime import datetime, timedelta, timezone
 from metar import Metar as MetarLib
@@ -77,6 +78,10 @@ class Metar:
 
     def get(self):
         return self.raw
+
+
+    def hasMetar(self):
+        return self.metar is not None
 
 
     def getInfo(self):
@@ -241,20 +246,24 @@ class MetarHistorical(Metar):
 
         logger.debug(f":fetch: url={url}")
         #with open("/Users/pierre/Developer/oscars/emitpy/src/emitpy/airspace/result.txt", "r") as response:  # urllib.request.urlopen(url) as response:
-        with urllib.request.urlopen(url) as response:
-            txt = response.read().decode("UTF-8")
-            logger.debug(f":fetch: {txt}")
-            # 201903312300 METAR OTHH 312300Z
-            start = f"{nowstr2} METAR {self.icao} {nowstr}"
-            logger.debug(f":fetch: start '{start}'")
-            metar = None
-            for line in re.findall(start+"(.*)", txt):
-                 metar = start+line
+
+        response = requests.get(url, cookies={'cookieconsent_status': 'dismiss'})
+        txt = response.text
+
+        # with urllib.request.urlopen(url) as response:
+        #     txt = response.read().decode("UTF-8")
+        logger.debug(f":fetch: {txt}")
+        # 201903312300 METAR OTHH 312300Z
+        start = f"{nowstr2} METAR {self.icao} {nowstr}"
+        logger.debug(f":fetch: start '{start}'")
+        metar = None
+        for line in re.findall(start+"(.*)", txt):
+             metar = start+line
             # logger.debug(f":fetchHistoricalMetar: search for '{start}(.*)': {metar}")
 
         if metar is None:
             return (False, "MetarHistorical::fetch: failed to get historical metar")
 
         self.raw = metar[len(nowstr2)+7:-1]
-        logger.debug(f":fetch: metar '{self.raw}'")
+        logger.debug(f":fetch: historical metar {self.moment_norm} '{self.raw}'")
         return self.parse()
