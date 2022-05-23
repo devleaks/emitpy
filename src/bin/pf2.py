9458758279fb8c66e56d39b5d3c0a00ddcb62822
Pierre Mareschal <pierre@devleaks.be>
2022-05-23 14:18:44 +0200

    Update pf2.py

--- src/bin/pf2.py
+++ src/bin/pf2.py
@@ -15,23 +15,23 @@ kn = fn.replace(".json", "").replace("-", ":").lower()
 with open(sys.argv[1]) as data_file:
     data = json.load(data_file)
     print(fn)
-    r.json().set('flightplans:'+kn, Path.root_path(), data)
+    r.json().set('flightplans:fpdb:'+kn, Path.root_path(), data)
 
 
-# flight plans (geojson)
-# for file in ../../data/managedairport/OTHH/flightplans/*.geojson; do python pf2.py $file; done
-fn = os.path.basename(sys.argv[1])
-kn = fn.replace(".geojson", "").replace("-", ":")
-with open(sys.argv[1]) as data_file:
-    data = json.load(data_file)
-    print(fn)
-    r.json().set('flightplans-geo:'+kn, Path.root_path(), data)
+# # flight plans (geojson)
+# # for file in ../../data/managedairport/OTHH/flightplans/*.geojson; do python pf2.py $file; done
+# fn = os.path.basename(sys.argv[1])
+# kn = fn.replace(".geojson", "").replace("-", ":")
+# with open(sys.argv[1]) as data_file:
+#     data = json.load(data_file)
+#     print(fn)
+#     r.json().set('flightplans:geojson:'+kn, Path.root_path(), data)
 
-# airports
-# for file in ../../data/airports/fpdb/*.json; do python pf2.py $file; done
-fn = os.path.basename(sys.argv[1])
-kn = fn.replace(".json", "").replace("-", ":").lower()
-with open(sys.argv[1]) as data_file:
-    data = json.load(data_file)
-    print(fn)
-    r.json().set('flightplans-apt:'+kn, Path.root_path(), data)
+# # airports
+# # for file in ../../data/airports/fpdb/*.json; do python pf2.py $file; done
+# fn = os.path.basename(sys.argv[1])
+# kn = fn.replace(".json", "").replace("-", ":").lower()
+# with open(sys.argv[1]) as data_file:
+#     data = json.load(data_file)
+#     print(fn)
+#     r.json().set('flightplans:airports:'+kn, Path.root_path(), data)
