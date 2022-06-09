3572b46f7d55d26496526f89b66811f7d1b172ba
Pierre Mareschal <pierre@devleaks.be>
2022-06-09 15:22:22 +0200

    pass key as param

--- src/web/security/genkey.py
+++ src/web/security/genkey.py
@@ -1,10 +1,11 @@
 import requests
 import json
+import sys
 
 BASE_URL="http://127.0.0.1:8000"
-SECRET_KEY='60a4dfdb-63a5-4481-a760-885a3a1c1474'
+
 
 response = requests.get(BASE_URL + "/auth/new",
-                        headers = {'secret-key': SECRET_KEY},
+                        headers = {'secret-key': sys.argv[1]},
                         data = json.dumps({"never_expires": True}))
 print(response.json())
--- src/web/security/testapikey.py
+++ src/web/security/testapikey.py
@@ -1,7 +1,7 @@
 import requests
+import sys
 
 BASE_URL="http://127.0.0.1:8000"
-API_KEY='1b646598-10b5-45cf-8f71-beb95035b9e4'
 
-response = requests.get(BASE_URL + "/airport/runways", headers = {'api-key': API_KEY})
+response = requests.get(BASE_URL + "/airport/runways", headers = {'api-key': sys.argv[1]})
 print(response.json())
