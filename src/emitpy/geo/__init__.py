0eb45d01715ff876a2d61b145d46af06942ca3da
Pierre Mareschal <pierre@devleaks.be>
2022-08-03 18:33:29 +0200

    added ls helper functions

--- src/emitpy/geo/__init__.py
+++ src/emitpy/geo/__init__.py
@@ -1,5 +1,6 @@
 from .features import FeatureWithProps, Location, ServiceParking, Ramp, Runway
 from .utils import mkPolygon, mkBbox, moveOn, line_intersect
 from .utils import asLineString, cleanFeature, cleanFeatures, printFeatures, findFeatures, getFeatureCollection, toTraffic
+from .utils import ls_length, ls_point_at
 from .kml import toKML
 from .movement import MovePoint, Movement
