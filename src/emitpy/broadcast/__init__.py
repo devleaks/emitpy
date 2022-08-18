c4fce737052c56b37b6e141938130db6afc3fe87
Pierre Mareschal <pierre@devleaks.be>
2022-08-18 17:33:10 +0200

    bumped version number

--- src/emitpy/broadcast/__init__.py
+++ src/emitpy/broadcast/__init__.py
@@ -2,4 +2,3 @@ from .queue import Queue
 from .enqueuetoredis import EnqueueToRedis
 from .broadcaster import Broadcaster, Hypercaster
 from .format import Format
-from .formatter import Formatter, LiveTrafficFormatter, FormatterFlat
