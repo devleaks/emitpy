7a8e16999b188133a484ad05842479ba2fe2ca50
Pierre Mareschal <pierre@devleaks.be>
2022-11-15 15:54:08 +0100

    Added Eurocontrol ATMAP for Metar

--- src/emitpy/airspace/__init__.py
+++ src/emitpy/airspace/__init__.py
@@ -1,4 +1,5 @@
 from .metar import Metar
+from .atmap import ATMAP
 from .airspace import Airspace, Restriction, NavAid, Terminal, Fix, AirwaySegment, SignificantPoint, CPIDENT
 from .xpairspace import XPAirspace
 from .procedure import CIFP
