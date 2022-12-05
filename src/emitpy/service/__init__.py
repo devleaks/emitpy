e8cb722c66a10cbff6ca0cced03d2788c4e72935
Pierre Mareschal <pierre@devleaks.be>
2022-12-05 17:49:56 +0100

    moved turnaround to flightservice

--- src/emitpy/service/__init__.py
+++ src/emitpy/service/__init__.py
@@ -3,5 +3,4 @@ from .mission import Mission
 from .equipment import Equipment, MissionVehicle
 from .servicemovement import ServiceMove
 from .missionmovement import MissionMove
-from .flightservices import FlightServices
-from .turnaround import Turnaround
+from .flightservices import FlightServices, Turnaround
