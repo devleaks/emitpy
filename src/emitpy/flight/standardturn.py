0f4e8ef7456c40128f1063417d1b1cf61b94c822
Pierre M <pierre@devleaks.be>
2023-09-24 19:01:06 +0200

    Added FLY OVER points. Works with constraints (segments size, turn angles)

--- src/emitpy/flight/standardturn.py
+++ src/emitpy/flight/standardturn.py
@@ -5,7 +5,7 @@ from math import pi
 
 from emitpy.geo.turf import Point, LineString, Feature, FeatureCollection
 from emitpy.geo.turf import destination, bearing, distance
-#from .movement import MovePoint
+from emitpy.geo import mk360
 
 logger = logging.getLogger("standard_turns")
 
@@ -13,19 +13,8 @@ logger = logging.getLogger("standard_turns")
 def sign(x):
     return -1 if x < 0 else (0 if x == 0 else 1)
 
-
-def turnRadius(speed):  # speed in m/s, returns radius in m
+def turnRadius(speed):  # speed in m/s, returns radius in m for standard turns (2 min)
     return 120 * speed / (2 * pi)
-
-
-def turn(bi, bo):
-    t = bi - bo
-    if t < 0:
-        t += 360
-    if t > 180:
-        t -= 360
-    return t
-
 
 def extend_line(line, pct=40):
     # Extended line direction need to be returned (opposite direction)
@@ -33,29 +22,25 @@ def extend_line(line, pct=40):
     # New 25/2/22: ... with a minimum of 20-40 km, because sometimes, segments are very short.
     # We noticed segments can sometimes be as long as 300km
     #
-    brng = bearing(Feature(geometry=Point(line.coordinates[0])), Feature(geometry=Point(line.coordinates[1])))
-    newdist = distance(Feature(geometry=Point(line.coordinates[0])), Feature(geometry=Point(line.coordinates[1])))
+    brng = bearing(line.coordinates[0], line.coordinates[1])
+    newdist = distance(line.coordinates[0], line.coordinates[1])
     dist = max(30, newdist * pct / 100)  # km
-    far0 = destination(Feature(geometry=Point(line.coordinates[0])), dist, brng + 180)
-    far0 = Feature(geometry=far0.getGeometry(), properties=far0.getProperties())
-    far1 = destination(Feature(geometry=Point(line.coordinates[1])), dist, brng)
-    far1 = Feature(geometry=far1.getGeometry(), properties=far1.getProperties())
+    far0 = destination(line.coordinates[0], dist, brng + 180)
+    far1 = destination(line.coordinates[1], dist, brng)
     return Feature(geometry=LineString([far1.geometry.coordinates, far0.geometry.coordinates]),
                    properties={
                        "name": f"B {brng}",
                        "bearing": brng
                    })
 
-
 def line_offset(line, offset):
-    p0 = Feature(geometry=Point(line.geometry.coordinates[0]))
-    p1 = Feature(geometry=Point(line.geometry.coordinates[1]))
-    brg = bearing(p0, p1)
+    p0 = line.geometry.coordinates[0]
+    p1 = line.geometry.coordinates[1]
+    brg = bearing(line.geometry.coordinates[0], line.geometry.coordinates[1])
     brg = brg - 90 # sign(offset) * 90
     d0 = destination(p0, offset, brg)
     d1 = destination(p1, offset, brg)
     return Feature(geometry=LineString([d0.geometry.coordinates, d1.geometry.coordinates]))
-
 
 def line_intersect(line1, line2):
     coords1 = line1.geometry.coordinates
@@ -87,7 +72,6 @@ def line_intersect(line1, line2):
 
     return None
 
-
 def line_arc(center, radius, start, end, steps=8):
     arc = []
     if end < start:
@@ -101,10 +85,21 @@ def line_arc(center, radius, start, end, steps=8):
     return arc
 
 
+# FLY BY
+#
+#
 def standard_turn_flyby(l0, l1, radius, precision=8):
+    def turn(bi, bo):
+        t = bi - bo
+        if t < 0:
+            t += 360
+        if t > 180:
+            t -= 360
+        return t
+
     local_debug = False
-    b_in = bearing(Feature(geometry=Point(l0.coordinates[1])), Feature(geometry=Point(l0.coordinates[0])))
-    b_out = bearing(Feature(geometry=Point(l1.coordinates[1])), Feature(geometry=Point(l1.coordinates[0])))
+    b_in = bearing(l0.coordinates[1], l0.coordinates[0])
+    b_out = bearing(l1.coordinates[1], l1.coordinates[0])
     turnAngle = turn(b_in, b_out)
 
     # Eliminate almost straight turns
@@ -124,8 +119,8 @@ def standard_turn_flyby(l0, l1, radius, precision=8):
             logger.debug(f"standard_turn: large turn (turn={turnAngle:f}°)")
 
     # Eliminate short segement turns (impossible)
-    d_in = distance(Feature(geometry=Point(l0.coordinates[1])), Feature(geometry=Point(l0.coordinates[0])))
-    d_out = distance(Feature(geometry=Point(l1.coordinates[1])), Feature(geometry=Point(l1.coordinates[0])))
+    d_in = distance(l0.coordinates[1], l0.coordinates[0])
+    d_out = distance(l1.coordinates[1], l1.coordinates[0])
 
     r = 1.5 * radius / 1000  # km
     if d_in < r or d_out < r:
@@ -166,3 +161,98 @@ def standard_turn_flyby(l0, l1, radius, precision=8):
         arc.reverse()
 
     return arc
+
+
+# FLY OVER
+#
+#
+def new_turn(bi, bo):
+    # Return turn angle between [-180, 180] relative to bi,
+    # negative angle is counter-clockwise (left) turn
+    if bi == bo:
+        return 0
+    bi = mk360(bi)
+    bo = mk360(bo)
+    left_turn = None
+    angle = 0
+    if bo > bi:
+        left_turn = False
+        angle = bo - bi
+    else:
+        left_turn = True
+        angle = bi - bo
+    if angle > 180:
+        angle = 360 - angle
+        left_turn = not left_turn
+    ret = (angle * -1.0) if left_turn else angle
+    return ret
+
+
+def new_line_arc(center, radius, start, end, direction, steps=8):
+    start = mk360(start)
+    end = mk360(end)
+    left_turn = direction < 0
+    arc = []
+    if left_turn:
+        step = (start - end) / steps
+        a = start
+        while a > end:
+            p = destination(center, radius, a)
+            arc.append(p)
+            a = a - step
+    else:
+        if end < start:
+            end = end + 360
+        step = (end - start) / steps
+        a = start
+        while a < end:
+            p = destination(center, radius, a + 180)
+            arc.append(p)
+            a = a + step
+    return arc
+
+
+def standard_turn_flyover(l0, l1, r):
+    #
+    # Standard turns manipulates angle [0,360[.
+    # pyturn manipulates angles [-180, 180]
+    #
+    p_start = Feature(geometry=Point(l0.coordinates[0]))
+    p_turn = Feature(geometry=Point(l0.coordinates[1]))
+    p_end = Feature(geometry=Point(l1.coordinates[1]))
+    b_in = bearing(p_start, p_turn)
+    b_out = bearing(p_turn, p_end)
+
+    turnAngle = new_turn(b_in, b_out)
+    turnAngleExtra = min(turnAngle * 1.5, 180)
+    b_out_old = b_out
+    b_out = b_in + turnAngleExtra
+    logger.debug(f"b_in={b_in}, b_out: {b_out_old}, turn={turnAngle}, extra={turnAngleExtra}, new_b_out: {b_out}")
+
+    # angle vertex
+    radius = r / 1000
+    v = Feature(geometry=Point(l0.coordinates[1]))
+    c1 = destination(v, radius, b_in + sign(turnAngle) * 90)
+    arc1 = new_line_arc(c1, radius, b_in+90, b_out+90, sign(turnAngle), 8)
+    if arc1 is None or len(arc1) < 1:
+        logger.warning(f"no arc 1")
+        arc1 = []
+        return [c1]
+
+    # from the last point, we draw a line to intersect the outline:
+    straight = destination(arc1[-1], 20*radius, b_out)
+    l2 = Feature(geometry=LineString([arc1[-1].geometry.coordinates, straight.geometry.coordinates]))
+    intersect = line_intersect(Feature(geometry=l1), l2)
+    if intersect is None:
+        logger.warning("no intersect")
+        return [c1, l2] + arc1
+    join = LineString([arc1[-1].geometry.coordinates, intersect.geometry.coordinates])
+    left = LineString([intersect.geometry.coordinates, l1.coordinates[1]])
+    arc2 = standard_turn_flyby(join, left, r)
+    if arc2 is None:
+        logger.warning(f"no arc 2")
+        arc2 = []
+
+    ret = [c1, l2] + arc1 + arc2
+    print(">>>", ret)
+    return ret
